b588381af69e4a3b94fa891bd8c584c500dea4a7
ycoupannec <yohann.coupannec@gmail.com>
2016-12-16 15:27:31 +0100

    add function for gen img

---
+++ include/fonction.php
@@ -0,0 +1,180 @@
+<?php
+define('EMPL_ORIGNAL', '../public/img/');
+define('EMPL_UPL', '../public/uploadImg/');
+
+
+/*echo "resultat ". $_REQUEST['image']."<br/>";*/
+
+
+$sizeTop = request('sizeTop');
+$sizeBot = request('sizeBot');
+$clrTop = request('clrTop');
+$clrBot = request('clrBot');
+$textTop = request('textTop');
+$textBot = request('textBot');
+$idImg = request('idImg');
+
+/*echo $type;
+echo $sizeTop;
+echo $sizeBot;
+echo $clrTop;
+echo $clrBot;
+echo $textTop;
+echo $textBot;
+*/
+
+
+if($idImg && $sizeTop && $sizeBot && $clrTop && $clrBot && $textTop && $textBot  ){
+	
+	$infoImg = recupImgSource($idImg);
+	$nomNouvImg = creerImage($infoImg,$textTop,$textBot,$clrTop,$clrBot,$sizeTop,$sizeBot);
+	$idNouvGen = insertImg($nomNouvImg,$textTop,$textBot,$clrTop,$clrBot,$sizeTop,$sizeBot,$infoImg['ID']);
+	echo $idNouvGen;
+}
+else{
+	echo "remplissez tous les champs";
+}
+
+function request($key){
+	 /*&& !empty(trim($_REQUEST[$key]))*/
+	if(isset($_REQUEST[$key])){
+		return $_REQUEST[$key];
+	}else{
+		return false;
+	}
+
+}
+function recupImgSource($idImg){
+	$sql= new SQLpdo();
+	$info=$sql->fetch("SELECT * FROM `memeImage` WHERE ID=:idImg", array(":idImg" => $idImg));
+	return $info;
+
+}
+
+
+function creerImage($info,$texteTop,$textBot,$clrTop,$clrBot,$sizeTop,$sizeBot){
+	$font=5;
+	$idUnique=uniqid();
+	//$font=loadFonts('myfont');
+	
+	$monImage = EMPL_ORIGNAL.$info['ID'].".".$info['type'];
+	$IDimg = 1;
+	switch ($info['type']) {
+		case 'jpeg':
+		case 'jpg': 
+			//header ("Content-type: image/jpeg");
+			$image = imagecreatefromjpeg($monImage);
+			break;
+		case 'png':
+			
+			$image = imagecreatefrompng($monImage);
+			break;
+		
+		case 'gif':
+			
+			$image = imagecreatefromgif($monImage);
+			break;
+		case 'svg':
+			return "Cas non géré pour le moment.";
+			break;
+		default:
+			return "Cas non géré pour le moment.";
+			break;
+	}
+	$x = imagesx($image);
+	$y = imagesy($image);
+	$colorTop = imagecolorallocate($image,255,255,255);
+	imagestring($image, $font, 0, 0,$texteTop,$colorTop);
+	/*, $clrTop*/
+
+	imagestring($image, $font, 150, 150,$textBot,$colorTop);
+	/*, $clrBot*/
+/*	$IDimg=;*/
+
+	switch ($info['type']) {
+		case 'jpeg':
+		case 'jpg':
+			imagejpeg($image,EMPL_UPL.$idUnique.".".$info['type']);
+			break;
+		case 'png':
+			imagepng($image,EMPL_UPL.$idUnique.".".$info['type']);
+			break;
+		case 'gif':
+			imagegif($image,EMPL_UPL.$idUnique.".".$info['type']);
+			break;
+		case 'svg':
+			
+			break;
+		
+		default:
+			# code...
+			break;
+	}
+	return $idUnique;
+
+}
+
+function loadFonts($nameFont){
+	switch ($nameFont) {
+		case 'myfont':
+			$font=imageloadfont('fonts/impact.ttf');
+
+			break;
+		
+		default:
+			# code...
+			break;
+	}
+	
+
+	return $font;
+}
+
+class SQLpdo {
+	function __CONSTRUCT($pass = false,$login=false, $adress=false,$db=false){
+		$config['login']=($login) ? $login :'marieb';
+		$config['mdp']= ($pass) ? $pass : '4V9FbQcYqq';
+		$config['adress']=($adress) ? $adress :'127.0.0.1';
+		$config['db']=($db) ? $db :'marieb';
+		
+
+		try {
+		    $this->db = new PDO('mysql:dbname='.$config['db'].';host='.$config['adress'].'', $config['login'], $config['mdp']);
+		} catch (PDOException $e) {
+		    echo 'Connexion échouée : ' . $e->getMessage();
+		}
+
+	}
+
+	function fetchAll($sql,$execute=null){
+		$sth = $this->db->prepare($sql);//prepare SQL request
+	    $sth->execute($execute);//execute la requette sql
+	    return $sth->fetchAll(PDO::FETCH_ASSOC);// recupère toutes les données
+	}
+
+	function insert($sql, $execute=null){
+		$sth = $this->db->prepare($sql);//prepare SQL request
+	    $sth->execute($execute);//execute la requette sql
+	    return  $this->db->lastInsertId();// recupère toutes les données
+	}
+
+	function fetch($sql,$execute=null){
+		$sth = $this->db->prepare($sql);//prepare SQL request
+	    $sth->execute($execute);//execute la requette sql
+	    return $sth->fetch(PDO::FETCH_ASSOC);// recupère toutes les données
+	}
+}
+
+function insertImg($img,$textTop,$textBot,$clrTop=null,$clrBot=null,$sizeTop=null,$sizeBot=null,$id){
+	echo $img." ".$textTop." ".$textBot." ".$clrTop." ".$clrBot." ".$sizeTop." ".$sizeBot." ".$id;
+	$sql= new SQLpdo();
+	$idGen=$sql->insert("INSERT INTO `memeGenerate` ( url, textTop, textBot, clrTop, clrBot, sizeTop, sizeBot, ID_memeImage) VALUES ( :url, :textTop, :textBot, :clrTop, :clrBot, :sizeTop, :sizeBot, :ID_memeImage)",
+		array(":url" => " ",':textTop'=> $textTop, ':textBot'=> $textBot, ':clrTop'=> $clrTop, ':clrBot'=> $clrBot, ':sizeTop'=> $sizeTop, ':sizeBot'=> $sizeBot, ':ID_memeImage'=> $id ));
+
+	
+
+	return $idGen;
+}
+
+
+
--- js/java.js
+++ js/java.js
@@ -1,6 +1,24 @@
-function geneImage($image,$type,$sizeTop,$sizeBot,$clrTop,$clrBot,$textTop,$textBot){
-	
+function geneImage(idImg,sizeTop,sizeBot,clrTop,clrBot,textTop,textBot){
+	/*console.log($image,$type,$sizeTop,$sizeBot,$clrTop,$clrBot,$textTop,textBot);*/
+	$.get('include/fonctionY.php',{
 
+		
+		sizeTop:sizeTop,
+		sizeBot:sizeBot,
+		clrTop:clrTop,
+		clrBot:clrBot,
+		textTop:textTop,
+		textBot:textBot,
+		idImg:idImg,
+		
+
+	})
+    .done(function(data) {
+        
+    })
+    .fail(function(data) {
+        alert('Error: ' + data);
+    });
 
 
 }
